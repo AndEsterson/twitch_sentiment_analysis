42922739c95da05c47d1f18692289d064cfe2b83
Andrew Esterson <andrewesterson1@gmail.com>
2023-12-30 11:00:37 +0000

    working python script for scraping logs using creds stored in aws. Needs logging fixed

--- container/main.py
+++ container/main.py
@@ -6,7 +6,6 @@ import random
 import string
 import json
 import boto3
-#from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 log_file = os.path.join(dir_path, 'chat.log')
@@ -14,28 +13,35 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s — %(message)s',
                     datefmt='%Y-%m-%d_%H:%M:%S',
                     handlers=[logging.FileHandler(log_file, encoding='utf-8')])
-#sia = SentimentIntensityAnalyzer()
 
-MAX_LOGIN_ATTEMPS = 10
+MAX_LOGIN_ATTEMPTS = 10
 LAMBDA_FUNCTION_NAME = 'twitch_refresh_credentials'
 
 def login():
     credentials = get_credentials()
-    sock = socket.socket()
-    sock.connect((SERVER,PORT))
     login_attempt_counter = 0
     while True:
-        sock.send(f"PASS {credentials['token']}\n".encode('utf-8'))
-        sock.send(f"NICK {credentials['nickname'}\n".encode('utf-8'))
-        sock.send(f"JOIN {credentials['channel'}\n".encode('utf-8'))initial_resp = sock.recv(2048).decode('utf-8')
+        sock = socket.socket()
+        sock.connect((credentials['server'],credentials['port']))
+        sock.send(f"PASS oauth:{credentials['access_token']}\n".encode('utf-8'))
+        sock.send(f"NICK {credentials['nickname']}\n".encode('utf-8'))
+        sock.send(f"JOIN {credentials['channel']}\n".encode('utf-8'))
+        initial_resp = sock.recv(2048).decode('utf-8')
         login_attempt_counter += 1
         if login_check(initial_resp):
-            break
+            return sock
         elif login_attempt_counter >= MAX_LOGIN_ATTEMPTS:
             return False
         else:
+            print('attempting to regenerate creds')
             regenerate_creds()
-   return True
+
+def login_check(initial_resp):
+    if 'GLHF' in initial_resp:
+        return True
+    else:
+        print(initial_resp)
+        return False
 
 def regenerate_creds():
     client = boto3.client('lambda')
@@ -50,7 +56,7 @@ def get_credentials():
     )
     return json.loads(response['Parameter']['Value'])
 
-def run_server:
+def run_server(sock):
     while True:
         resp = sock.recv(2048).decode('utf-8')
 
@@ -60,9 +66,15 @@ def run_server:
 
         elif len(resp) > 0:
             logging.info((resp.replace('\n', ' ').replace('\r', ' ')))
+    return True
 
 def main():
-    if login():
-        run_server():
+    sock = login()
+    if sock:
+        print('running_server')
+        run_server(sock)
     else:
         raise Exception(f'login failed after {MAX_LOGIN_ATTEMPTS} attempts')
+
+if __name__ == '__main__':
+    main()
