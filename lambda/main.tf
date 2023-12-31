ec7e430b6fd6faad80f260630e632c4919b84b19
Andrew Esterson <andrewesterson1@gmail.com>
2023-12-31 14:18:21 +0000

    switched to lambda for chat scraper

--- lambda/main.tf
+++ lambda/main.tf
@@ -28,7 +28,76 @@ data "aws_iam_policy_document" "allow_ssm" {
   }
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
+data "aws_iam_policy_document" "allow_ssm_read" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter"
+            ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "allow_s3" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+                "ssm:PutParameter",
+                "ssm:DeleteParameter",
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter",
+                "ssm:DeleteParameters"
+            ]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "allow_twitch_logs_s3" {
+    statement {
+            effect = "Allow"
+            actions = ["s3:ListBucket"]
+            resources = ["arn:aws:s3:::twitch-scraper-logs"]
+        }
+    statement {
+            effect = "Allow"
+            actions = ["s3:*Object"]
+            resources = ["arn:aws:s3:::twitch-scraper-logs/*"]
+        }
+}
+
+data "aws_iam_policy_document" "allow_invoke_lambda" {
+    statement {
+            effect = "Allow"
+            actions = ["lambda:InvokeFunction"]
+            resources = ["arn:aws:lambda:eu-west-2:192225688557:function:twitch_refresh_credentials"]
+        }
+}
+
+resource "aws_iam_role" "iam_for_lambda_scraper" {
+  name               = "lambda_twitch_scraper_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  inline_policy {
+    name = "allow_ssm_read"
+    policy = data.aws_iam_policy_document.allow_ssm_read.json
+  }
+  inline_policy {
+   name = "allow_twitch_logs_s3"
+   policy = data.aws_iam_policy_document.allow_twitch_logs_s3.json
+  }
+  inline_policy {
+   name = "allow_invoke_lambda"
+   policy = data.aws_iam_policy_document.allow_invoke_lambda.json
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda_credentials" {
   name               = "twitch_refresh_credentials_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   inline_policy {
@@ -37,20 +106,37 @@ resource "aws_iam_role" "iam_for_lambda" {
   }
 }
 
-data "archive_file" "lambda" {
+data "archive_file" "lambda_credentials" {
   type        = "zip"
   source_file = "refresh_credentials.py"
-  output_path = "lambda_function_payload.zip"
+  output_path = "lambda_credentials_payload.zip"
 }
 
 resource "aws_lambda_function" "twitch_refresh_credentials" {
-  filename      = "lambda_function_payload.zip"
+  filename      = "lambda_credentials_payload.zip"
   function_name = "twitch_refresh_credentials"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          = aws_iam_role.iam_for_lambda_credentials.arn
   handler       = "refresh_credentials.lambda_handler"
   architectures  = ["arm64"]
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = data.archive_file.lambda_credentials.output_base64sha256
 
   runtime = "python3.8"
 
+}
+
+data "archive_file" "lambda_twitch_scraper" {
+  type        = "zip"
+  source_file = "twitch_scraper.py"
+  output_path = "lambda_scraper_payload.zip"
+}
+
+resource "aws_lambda_function" "twitch_log_scraper" {
+  filename      = "lambda_scraper_payload.zip"
+  function_name = "twitch_log_scraper"
+  role          = aws_iam_role.iam_for_lambda_scraper.arn
+  handler       = "twitch_scraper.lambda_handler"
+  timeout       = 300
+  architectures  = ["arm64"]
+  source_code_hash = data.archive_file.lambda_twitch_scraper.output_base64sha256
+  runtime = "python3.8"
 }
---
+++ lambda/twitch_scraper.py
@@ -0,0 +1,95 @@
+import socket
+import logging
+import logging.handlers
+import time
+import json
+import boto3
+
+LOG_FILE = '/tmp/chat.log'
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s — %(message)s',
+                    datefmt='%Y-%m-%d_%H:%M:%S')
+chat_logger = logging.getLogger('chat_logger')
+chat_handler = logging.handlers.WatchedFileHandler(LOG_FILE, encoding='utf-8')
+chat_handler.setFormatter(logging.Formatter(fmt='%(asctime)s — %(message)s', datefmt='%Y-%m-%d_%H:%M:%S'))
+chat_logger.addHandler(chat_handler)
+
+MAX_LOGIN_ATTEMPTS = 3
+LAMBDA_TIME_CUTOFF = 20000
+S3_BUCKET = "twitch-scraper-logs"
+LAMBDA_FUNCTION_NAME = 'twitch_refresh_credentials'
+S3_BUCKET_NAME = 'twitch_scraper'
+
+def login(event):
+    credentials = get_credentials()
+    credentials.update(event)
+    login_attempt_counter = 0
+    while True:
+        sock = socket.socket()
+        sock.connect((credentials['server'],credentials['port']))
+        sock.send(f"PASS oauth:{credentials['access_token']}\n".encode('utf-8'))
+        sock.send(f"NICK {credentials['nickname']}\n".encode('utf-8'))
+        sock.send(f"JOIN {credentials['channel']}\n".encode('utf-8'))
+        initial_resp = sock.recv(2048).decode('utf-8')
+        login_attempt_counter += 1
+        if login_check(initial_resp):
+            return sock
+        elif login_attempt_counter >= MAX_LOGIN_ATTEMPTS:
+            return False
+        else:
+            logging.info('attempting to regenerate creds')
+            regenerate_credentials()
+            credentials = get_credentials()
+
+def login_check(initial_resp):
+    if 'GLHF' in initial_resp:
+        return True
+    else:
+        logging.info(initial_resp)
+        return False
+
+def regenerate_credentials():
+    client = boto3.client('lambda')
+    logging.info('calling lambda')
+    response = client.invoke(FunctionName=LAMBDA_FUNCTION_NAME)
+    return response
+
+def get_credentials():
+    client = boto3.client('ssm')
+    response = client.get_parameter(
+        Name='twitch_credentials'
+    )
+    return json.loads(response['Parameter']['Value'])
+
+def upload_logs(local_file_path, s3_bucket):
+    s3_key = "raw_logs/" + str(int(time.time())) + "_" + 'chat.log'
+    s3 = boto3.client("s3")
+    s3.upload_file(local_file_path, s3_bucket, s3_key)
+    logging.info(f"uploaded logs to {s3_key}")
+    return True
+
+def run_server(sock, context):
+    while True:
+        if context.get_remaining_time_in_millis() < LAMBDA_TIME_CUTOFF:
+            return upload_logs(LOG_FILE, S3_BUCKET)
+        resp = sock.recv(2048).decode('utf-8')
+
+        if resp.startswith('PING'):
+            sock.send("PONG\n".encode('utf-8'))
+            logging.info('sent pong')
+
+        elif len(resp) > 0:
+            chat_logger.info((resp.replace('\n', ' ').replace('\r', ' ')))
+    return False
+
+def lambda_handler(event, context):
+    sock = login(event)
+    if sock:
+        logging.info('running_server')
+        if run_server(sock, context):
+            return f'Upload complete with event {event}, exiting'
+        else:
+            return f'Did not upload with event {event}, exiting'
+    else:
+        raise Exception(f'login failed after {MAX_LOGIN_ATTEMPTS} attempts')
+
